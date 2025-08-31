4303fa8635a083f513f86f74d42589947478486a
jablonskii <matheus.jablonski@aluno.farroupilha.ifrs.edu.br>
2025-08-31 16:14:36 -0300

    Add files via upload

---
+++ Roleta_Matheus-Kalindi-Caio/app/models.py
@@ -0,0 +1,18 @@
+from app import db, login_manager, bcrypt
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(60), nullable=False)
+    bankroll = db.Column(db.Float, nullable=False, default=1000.0)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
